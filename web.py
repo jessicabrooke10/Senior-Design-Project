8c91ccdc82c29fa8c8dc6f54b28524885f9dfbe9
Jessica <jessicabjanko@gmail.com>
2024-04-01 22:12:03 -0400

    Update

--- web.py
+++ web.py
@@ -4,10 +4,12 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+import plotly.io as pio
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn.cluster import DBSCAN
+import plotly.graph_objects as go
 from sklearn.preprocessing import StandardScaler
 from prophet import Prophet
 from statsmodels.tsa.seasonal import STL
@@ -29,119 +31,93 @@ app = Flask(__name__)
 chunksize = 100000
 
 # Generate Cluster Plot
-def generate_plot(day):
-
+def generate_plot(day, start_time, end_time):
+    # Read chosen day of the week file
     df = pd.read_csv("days/hourly_aggregated_"+ day +".csv")
-
+    
+    # Change time columns to datetime format
     df['_time'] = pd.to_datetime(df['_time'])
 
-    start_time = pd.to_datetime('09:00:00').time()
-    end_time = pd.to_datetime('15:00:00').time()
+    # Set a start time and end time for clustering graph
+    start_time = pd.to_datetime(start_time).time()
+    end_time = pd.to_datetime(end_time).time()
 
+    # Filter rows based on the times selected above
     df = df[(df['_time'].dt.time >= start_time) & (df['_time'].dt.time <= end_time)]
 
     # Convert dates to numerical representation (Unix timestamp)
     df['_time'] = pd.to_datetime(df['_time']).astype('int64') // 10**9
     
-    if day == 'monday':
-        eps_value = 5
-        groups = 3
-    elif day == 'tuesday':
-        eps_value = 12
-        groups = 2
-    elif day == 'wednesday':
-        eps_value = 15
-        groups = 2
-    elif day == 'thursday':
-        eps_value = 10
-        groups = 2
-    elif day == 'friday':
-        eps_value = 13
-        groups = 2
-    elif day == 'saturday':
-        eps_value = 15
-        groups = 2
-    elif day == 'sunday':
-        eps_value = 15
-        groups = 3
-
-    # Set a start time and end time for clustering graph
-    start_time = pd.to_datetime('09:00:00').time()
-    end_time = pd.to_datetime('15:00:00').time()
-
     # Take CPU percentile data and _time (only columns used for clustering)
     DBSCAN_data = df[['_time', 'CPU_95th_Perc']]
 
     # Run DBScan algorithm on above columns
+    eps_value = 6
+    groups = 2
     clustering = DBSCAN(eps=eps_value, min_samples=groups).fit(DBSCAN_data)
     DBSCAN_dataset = DBSCAN_data.copy()
     DBSCAN_dataset.loc[:,'Cluster'] = clustering.labels_ 
 
-    DBSCAN_dataset.Cluster.value_counts().to_frame()
+    # Group the data by cluster and calculate cluster-level statistics
+    cluster_stats = DBSCAN_dataset[DBSCAN_dataset['Cluster']!=-1].groupby('Cluster').agg({
+        'CPU_95th_Perc': ['min', 'max', 'mean', 'median', 'std']
+    })
 
-    outliers = DBSCAN_dataset[DBSCAN_dataset['Cluster']==-1]
+    cluster_counts = DBSCAN_dataset[DBSCAN_dataset['Cluster']!=-1]['Cluster'].value_counts()
 
-    fig2, axes = plt.subplots(1,figsize=(12,5))
+    # Create hover text for each cluster
+    hover_text = []
+    cluster_labels = clustering.labels_[clustering.labels_ != -1]
 
-    set2_palette = sns.color_palette("Set2")
-
-    custom_palette = set2_palette + [
-        "#1f77b4",  # blue
-        "#ff7f0e",  # orange
-        "#2ca02c",  # green
-        "#d62728",  # red
-        "#9467bd",  # purple
-        "#8c564b",  # brown
-        "#e377c2",  # pink
-    ]
-
-    sns.scatterplot(data=DBSCAN_dataset[DBSCAN_dataset['Cluster']!=-1], x='_time', y='CPU_95th_Perc', hue='Cluster', ax=axes, palette=custom_palette, legend='full', s=200)
-
-    plt.title('CPU 95th Percentile Over Time - '+day)
-
-    axes.scatter(outliers['_time'], outliers['CPU_95th_Perc'], s=10, label='outliers', c="k")
-
-    axes.legend()
-
-    tick_labels = [pd.to_datetime(t, unit='s').strftime('%H:%M') for t in axes.get_xticks()]
-    axes.set_xticks(axes.get_xticks())
-    axes.set_xticklabels(tick_labels)
-
-    df['_time'] = pd.to_datetime(df['_time'], unit='s')
-
-    df['_time'] = df['_time'].dt.strftime('%H:%M')
-
-    plt.setp(axes.get_legend().get_texts(), fontsize='12')
-
-    # Change times back from seconds to HH:MM format
-    tick_labels = [pd.to_datetime(t, unit='s').strftime('%H:%M') for t in axes.get_xticks()]
-    axes.set_xticklabels(tick_labels)
-
-    # Following code takes the graph selected by user and creates seperate csv files of each cluster (used to see which servers belong to which groups)
-
-    '''
-    cluster_labels = clustering.labels_
-
-    cluster_dataframes = {}
     for cluster_id in set(cluster_labels):
-        if cluster_id != -1:  # Exclude outliers (cluster label = -1)
-            cluster_df = df[cluster_labels == cluster_id]
-            cluster_dataframes[cluster_id] = cluster_df
+        # Retrieve statistics for the current cluster ID
+        stats = cluster_stats.loc[cluster_id]
+        
+        # Get count of points in the cluster
+        count = cluster_counts.get(cluster_id, 0)
+        
+        # Append hover text for the current cluster
+        hover_text.append(f'Cluster: {cluster_id} <br> CPU Min: {stats["CPU_95th_Perc", "min"].round(2)} <br> CPU Max: {stats["CPU_95th_Perc", "max"].round(2)} <br> CPU Mean: {stats["CPU_95th_Perc", "mean"].round(2)} <br> CPU Median: {stats["CPU_95th_Perc", "median"].round(2)} <br> CPU STD: {stats["CPU_95th_Perc", "std"].round(2)} <br> Count: {count}')
 
-    for cluster_id, cluster_df in cluster_dataframes.items():
-        cluster_df.to_csv(f'cluster_{cluster_id}.csv', index=False)
-    '''
-    # Create BytesIO object to store plot image data
-    plot_io = BytesIO()
+    # Create a Plotly Figure object
+    fig = go.Figure()
 
-    # Saves figure to the BytesIO obj in PNG format
-    plt.savefig(plot_io, format='png')
-    plot_io.seek(0)
+    # Iterate over unique cluster IDs
+    for cluster_id in set(cluster_labels):
+        cluster_data = DBSCAN_dataset[DBSCAN_dataset['Cluster']!=-1][DBSCAN_dataset[DBSCAN_dataset['Cluster']!=-1]['Cluster'] == cluster_id]
 
-    # Encode image data as base64 for transmission
-    plot_base64 = base64.b64encode(plot_io.getvalue()).decode('utf-8')
-    plt.close()
+        # Extract x and y data for the current cluster
+        x_cluster = cluster_data['_time']
+        y_cluster = cluster_data['CPU_95th_Perc']
+        hover_text_cluster = hover_text[cluster_id]  # Generate hover text for the current cluster
+        
+        # Add scatter trace for the current cluster
+        fig.add_trace(go.Scatter(
+            x=x_cluster,
+            y=y_cluster,
+            mode='markers',
+            name=f'Cluster: {cluster_id}',
+            marker=dict(color=np.random.rand(3,)),
+            hovertext=hover_text_cluster  # Specify hover text for each data point in the current cluster
+        ))
 
+    # Customize the layout of the figure
+    fig.update_layout(
+        title='CPU 95th Percentile Over Time - '+ day,
+        xaxis_title="Time",
+        yaxis_title="CPU %"
+    )
+    x_data = DBSCAN_dataset[DBSCAN_dataset['Cluster']!=-1]['_time']
+    #change times back from seconds to HH:MM format
+    fig.update_layout(
+        xaxis=dict(
+            tickmode='array',  # Use 'array' mode for custom tick text
+            tickvals=x_data,  # Assuming your_data['x_column'] contains the tick values
+            ticktext=[pd.to_datetime(t, unit='s').strftime('%H:%M') for t in x_data]  # Convert tick values to HH:MM format
+            )
+        )
+    image_bytes = pio.to_image(fig, format='png')
+    plot_base64 = base64.b64encode(image_bytes).decode('utf-8')
     return plot_base64
 
 # Generate Prophet Plots
@@ -360,12 +336,24 @@ def choose_graph():
     if request.method == 'POST':
         graph = request.json.get('graph')
         return jsonify({'graph': graph})
-
+    
+def convert_to_lists(obj):
+        if isinstance(obj, dict):
+            return {k: convert_to_lists(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [convert_to_lists(item) for item in obj]
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return obj
+        
 @app.route('/run-cluster-graph', methods=['POST'])
 def run_cluster_graph():
     if request.method == 'POST':
         day = request.json.get('day')
-        plot_data = generate_plot(day)
+        start_time = request.json.get('start_time')
+        end_time = request.json.get('end_time')
+        plot_data = generate_plot(day, start_time, end_time)
         return jsonify({'plot_data': plot_data})
 
 @app.route('/', methods=['GET', 'POST'])
